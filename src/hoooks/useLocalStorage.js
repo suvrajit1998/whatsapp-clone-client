5180a5a87ab6a4f44e72d3a4c63dba3c2fc32702
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-09-24 09:53:25 +0530

    fixing localstorage prblme

--- src/hoooks/useLocalStorage.js
+++ src/hoooks/useLocalStorage.js
@@ -5,8 +5,10 @@ const PREFIX = "whatsapp-clone-";
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key;
   const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey);
-    if (jsonValue != null) return JSON.parse(jsonValue);
+    const { jsonValue, status } = localStorage.getItem(prefixedKey);
+    if (status === "success") {
+      if (jsonValue != null) return JSON.parse(jsonValue);
+    }
     if (typeof initialValue === "function") {
       return initialValue();
     } else {
